5410e9a5bfd82d43784e829ac67430c808a95cd9
Gun-Hee-SON <songunhee5426@gmail.com>
2024-07-08 15:39:54 +0900

    ver_02
    백엔드 부분 공부하는 부분
    https://wikidocs.net/160890

--- SON/sbb/src/main/java/com/mysite/sbb/Answer.java
+++ SON/sbb/src/main/java/com/mysite/sbb/Answer.java
@@ -7,23 +7,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+
+
 
 @Data
 @Entity
-public class Question {
+public class Answer { 
 	
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Integer id;
 
-    @Column(length = 200)
-    private String subject;
+    @Column(columnDefinition = "TEXT") 
+    private String content; 
 
-    @Column(columnDefinition = "TEXT")
-    private String content;
+    private LocalDateTime createDate; 
 
-    private LocalDateTime createDate;
-    
-
+    @ManyToOne 
+    private Question question; 
 }
--- SON/sbb_01/src/main/java/com/mysite/sbb_01/Answer.java
+++ SON/sbb_01/src/main/java/com/mysite/sbb_01/Answer.java
@@ -1,17 +1,21 @@
-package com.mysite.sbb;
+package com.mysite.sbb_01;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
 @Entity
-public class Question {
+public class Answer {
 	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,6 +28,14 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate;
+    
+    
+    @ManyToOne
+    private Question question;
+    
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) 
+    private List<Answer> answerList; 
     
 
 }
