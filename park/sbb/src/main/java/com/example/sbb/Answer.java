2347a09c913c3a374d4ea9b373d2e6b85b37175e
rktndnjseh <park.castle3@gmail.com>
2024-07-08 15:57:36 +0900

    course_v1

---
+++ park/sbb/src/main/java/com/example/sbb/Answer.java
@@ -0,0 +1,28 @@
+package com.example.sbb;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+@Getter 
+@Setter 
+@Entity 
+public class Answer { 
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT") 
+    private String content; 
+
+    private LocalDateTime createDate; 
+
+    @ManyToOne 
+    private Question question; 
+}
---
+++ park/sbb/src/main/java/com/example/sbb/sbbApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class sbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(sbbApplication.class, args);
+	}
+
+}
