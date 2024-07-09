a33fcd71bb2eb00a38d8c2dedd09f35b008d4d15
nanakyeong <129843360+nanakyeong@users.noreply.github.com>
2024-07-09 13:03:12 +0900

    스프링 부트 게시판

---
+++ leenakyeong/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -0,0 +1,37 @@
+package com.mysite.sbb.question;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DataNotFoundException;
+
+@Service
+public class QuestionService {
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+	public Question getQuestion(Integer id) {  
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+	}
+	
+	public void create(String subject, String content) {
+		Question q = new Question();
+		q.setSubject(subject);
+		q.setContent(content);
+		q.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q);
+	}
+	
+	public void delete(Integer id) {
+		questionRepository.deleteById(id);
+	}
+}
