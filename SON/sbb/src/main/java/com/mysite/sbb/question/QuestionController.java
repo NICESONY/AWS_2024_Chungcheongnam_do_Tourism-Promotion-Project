b3c978c82540dccace0c4c4782f6113584083de4
Gun-Hee-SON <songunhee5426@gmail.com>
2024-07-09 11:33:45 +0900

    2024_0709_ver02
    답변 하는 부분과 질문하는 부분 질문하고 댓글다는 것도 추가 되었다.
    
    부트스트랩으로 디자인 실시

--- SON/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ SON/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -38,6 +40,21 @@ public class QuestionController {
         model.addAttribute("question", question);
     	
     	return "question_detail";
+    }
+    	
+    @GetMapping("/create")
+    public String questionCreate() {
+       return "question_form";
+        }
+    
+    
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
         
+    	// TODO 질문을 저장한다.
+    	this.questionService.create(subject, content);
+        
+    	return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+    	// redirect 주소창에 던지는 것 html을 호출하는 것 아님 경우의 수는 2개 html이냔 redirectㅑ
     }
 }
--- SON/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ SON/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb.question;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,14 @@ public class QuestionService {
         } else { 
             throw new DataNotFoundException("question not found");
         }
+        
+    }
+	
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
